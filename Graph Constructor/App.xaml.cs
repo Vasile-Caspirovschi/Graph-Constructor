d09556b3d5c832b2bd273bfab1e8ae260bf16752
Vasile-Caspirovschi <97791123+Vasile-Caspirovschi@users.noreply.github.com>
2024-01-25 19:40:18 +0200

    Removing the global exception handler

--- Graph Constructor/App.xaml.cs
+++ Graph Constructor/App.xaml.cs
@@ -13,9 +13,9 @@ namespace Graph_Constructor
         }
         private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
         {
-            var exception = e.Exception;
-            MessageBox.Show(exception.Message);
-            e.Handled = true;
+            //var exception = e.Exception;
+            //MessageBox.Show(exception.Message);
+            //e.Handled = true;
         }
     }
 }
